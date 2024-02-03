8b33f4340a54ab1c2894296e098e07dc0f32cc99
pidtchay <pidtchay@gmail.com>
2024-02-03 21:36:33 +0300

    PDTCH-001 Добавил принудительное сохранение при переходе в фоновый режим

--- ToDoAppCoreDataIOS/ToDoAppCoreDataIOSApp.swift
+++ ToDoAppCoreDataIOS/ToDoAppCoreDataIOSApp.swift
@@ -10,11 +10,17 @@ import SwiftUI
 @main
 struct ToDoAppCoreDataIOSApp: App {
     let persistenceController = PersistenceController.shared
+    @Environment(\.scenePhase) var scenePhase
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .onChange(of: scenePhase) { oldValue, newValue in
+                    if newValue == .background {
+                        persistenceController.save()
+                    }
+                }
         }
     }
 }
